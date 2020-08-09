6fa0c5efa73339d4abbc3bd5477e2430bfba9769
Andrey <spiridonov.new@gmail.com>
2020-08-10 01:08:57 +0300

    add home page content template

--- src/pages/Home/Home.page.tsx
+++ src/pages/Home/Home.page.tsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const HomePage: React.FC = () => {
-  return <div>HomePage</div>;
+  return (
+    <div className="content">
+      <div className="content__topline topline">
+        <div className="topline__title">Список задач</div>
+
+        <div className="topline__button"></div>
+      </div>
+
+      <div className="content__todo-list todo-list"></div>
+    </div>
+  );
 };
 
 export default HomePage;
